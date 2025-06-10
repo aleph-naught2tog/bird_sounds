48c11e531977b35b44b65d871ef2fd2809e1cc7f
Max Cerrina <mrcerrina@gmail.com>
2025-06-10 06:12:13 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function preload() {
+  console.debug("preloaded")
+}
+
+function setup() {
+  console.debug("setup")
+
+  createCanvas(windowWidth / 2, windowHeight / 2);
+  background('plum');
+}
